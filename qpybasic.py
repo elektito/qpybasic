c68bc0419df272297269fa20cfc5a02626219c87
Mostafa Razavi <mostafa@sepent.com>
2019-04-28 04:36:25 +0200

    Add typing to all expressions and values.
    Both numeric literals and variables have types now. The types of
    expressions is also kept track of, and properly typed instructions are
    generated.

--- qpybasic.py
+++ qpybasic.py
@@ -46,6 +46,26 @@ def get_variable(prefix, typespec):
     name = '__{}_{}'.format(prefix, variables[prefix])
     return Var(name, typespec)
 
+def get_literal_typespec(literal):
+    if literal.startswith('"'): # string literal
+        return '$'
+    elif literal[-1] in '!#%&': # numeric literal with typespec
+        # sanity check first
+        if '.' in literal and literal[-1] not in '!#':
+            raise RuntimeError('Invalid numeric literal: {}'.format(literal))
+        return literal[-1]
+    else: # numeric literal without typespec
+        if '.' in literal: # single or double
+            return '#' # for now, consider all float literals as double
+        else: # integer or long
+            v = int(literal)
+            if -32768 <= v < 32768:
+                return '%'
+            elif -2**31 <= v < 2**31:
+                return '&'
+            else:
+                raise RuntimeError('Integer value out of possible range.')
+
 def flatten_tree(t):
     ret = []
     for i in t:
@@ -63,6 +83,41 @@ def flatten_tree(t):
             raise RuntimeError('Got something unexpected: {}'.format(repr(i)))
 
     return ret
+
+def get_conv_instr(typespec1, typespec2):
+    return Instr('conv' + typespec1 + typespec2)
+
+def binary_op(op, items):
+    (t1, instrs1), (t2, instrs2) = items
+    if t1 == t2:
+        t = t1
+    else:
+        t = {
+            frozenset({'!', '#'}): '#',
+            frozenset({'%', '&'}): '&',
+            frozenset({'%', '!'}): '!',
+            frozenset({'%', '#'}): '#',
+            frozenset({'&', '!'}): '!',
+            frozenset({'&', '#'}): '#',
+        }[frozenset({t1, t2})]
+
+    instrs = instrs1
+    if t1 != t:
+        instrs += [get_conv_instr(t1, t)]
+
+    instrs += instrs2
+    if t2 != t:
+        instrs += [get_conv_instr(t2, t)]
+
+    instrs += [Instr(op + t)]
+
+    return t, instrs
+
+def compare_op(op, items):
+    t, instrs = binary_op('sub', items)
+    instrs += [get_conv_instr(t, '%'),
+               Instr(op)]
+    return '%', instrs
 
 class MyC(Transformer):
     def start(self, items):
@@ -107,10 +162,17 @@ class MyC(Transformer):
 
         if items[5].type == 'STEP_KW':
             _, var, start, _, end, _, step, _, body, _ = items
-            step_instrs = step
+            step_type, step_instrs = step
+            if step_type == '$':
+                raise RuntimeError('Type mismatch.')
+            elif step_type != var.typespec:
+                step_instrs += [get_conv_instr(step_type, var.typespec)]
         else:
             _, var, start, _, end, _, body, _ = items
-            step_instrs = [Instr('pushi', 1)]
+            step_instrs = [Instr('pushi' + var.typespec, 1)]
+
+        if var.typespec == '$':
+            raise RuntimeError('Type mismatch.')
 
         start_label = get_label('for_start')
         end_label = get_label('for_end')
@@ -118,13 +180,25 @@ class MyC(Transformer):
         end_var = get_variable('for_end_var', var.typespec)
         step_var = get_variable('for_step_var', var.typespec)
 
+        start_type, start_instrs = start
+        if start_type == '$':
+            raise RuntimeError('Type mismatch.')
+        elif start_type != var.typespec:
+            start_instrs += [get_conv_instr(start_type, var.typespec)]
+
+        end_type, end_instrs = end
+        if end_type == '$':
+            raise RuntimeError('Type mismatch.')
+        elif end_type != var.typespec:
+            end_instrs += [get_conv_instr(end_type, var.typespec)]
+
         return \
-            start + \
-            [Instr('popl', var.qual_name())] + \
-            end + \
-            [Instr('popl', end_var.qual_name())] + \
+            start_instrs + \
+            [Instr('storel' + var.typespec, var.qual_name())] + \
+            end_instrs + \
+            [Instr('storel' + end_var.typespec, end_var.qual_name())] + \
             step_instrs + \
-            [Instr('popl', step_var.qual_name()),
+            [Instr('storel' + step_var.typespec, step_var.qual_name()),
              Label(start_label),
              Instr('pushl', var.qual_name()),
              Instr('pushl', end_var.qual_name()),
@@ -134,20 +208,22 @@ class MyC(Transformer):
             [Instr('pushl', step_var.qual_name()),
              Instr('pushl', var.qual_name()),
              Instr('add'),
-             Instr('popl', var.qual_name()),
+             Instr('storel' + var.typespec, var.qual_name()),
              Instr('jmp', start_label),
              Label(end_label)]
 
     def if_block(self, items):
         # IF, cond, THEN, NEWLINE, then_body, rest, END, IF
-        _, cond, _, _, then_body, rest, _, _ = items
+        _, (cond_type, cond_instrs), _, _, then_body, rest, _, _ = items
+        if cond_type != '%':
+            cond_instrs += [get_conv_instr(conv_type, '%')]
 
         endif_label = get_label('endif')
 
         if rest:
             label, rest = rest
             rest = [Instr('jmp', endif_label) if r == 'JMP_TO_ENDIF' else r for r in rest]
-            return cond + [Instr('jmpf', label)] + then_body + [Instr('jmp', endif_label), Label(label)] + rest + [Label(endif_label)]
+            return cond_instrs + [Instr('jmpf', label)] + then_body + [Instr('jmp', endif_label), Label(label)] + rest + [Label(endif_label)]
         else:
             return cond + [Instr('jmpf', endif_label)] + then_body + [Label(endif_label)]
 
@@ -190,8 +266,20 @@ class MyC(Transformer):
     def let_stmt(self, items):
         if len(items) == 3:
             items = items[1:]
-        return [items[1],
-                Instr('storel', items[0].qual_name())]
+
+        expr_type, expr_instrs = items[1]
+        var = items[0]
+        instrs = expr_instrs
+
+        if expr_type == var.typespec:
+            pass
+        elif expr_type in '!#%&' and var.typespec in '!#%&':
+            instrs += [get_conv_instr(expr_type, var.typespec)]
+        else:
+            raise RuntimeError('Type mismatch.')
+
+        instrs += [Instr('storel' + var.typespec, var.qual_name())]
+        return instrs
 
     def print_stmt(self, items):
         args = []
@@ -201,7 +289,7 @@ class MyC(Transformer):
             elif isinstance(a, list):
                 args += a
             elif isinstance(a, Token):
-                args.append(Instr('pushi', '"{}"'.format(a.value)))
+                args.append(Instr('pushi$', '"{}"'.format(a.value)))
             else:
                 RuntimeError('Something unexpected sent to PRINT.')
         return args + [Instr('call', '__print', len(items) - 1)]
@@ -212,48 +300,52 @@ class MyC(Transformer):
 
     def expr_lt(self, items):
         assert len(items) == 2
-        return items + [Instr('lt')]
+        return compare_op('lt', items)
 
     def expr_gt(self, items):
         assert len(items) == 2
-        return items + [Instr('gt')]
+        return compare_op('gt', items)
 
     def expr_le(self, items):
         assert len(items) == 2
-        return items + [Instr('le')]
+        return compare_op('le', items)
 
     def expr_ge(self, items):
         assert len(items) == 2
-        return items + [Instr('ge')]
+        return compare_op('ge', items)
 
     def expr_eq(self, items):
         assert len(items) == 2
-        return items + [Instr('eq')]
+        return compare_op('eq', items)
 
     def expr_ne(self, items):
         assert len(items) == 2
-        return items + [Instr('ne')]
+        return compare_op('ne', items)
 
     def add_expr(self, items):
-        return sum(items, [])
+        assert len(items) == 1
+        return items[0]
 
     def expr_add(self, items):
         assert len(items) == 2
-        return items + [Instr('add')]
+        return binary_op('add', items)
 
     def expr_sub(self, items):
         assert len(items) == 2
-        return items + [Instr('sub')]
+        return binary_op('sub', items)
 
     def mult_expr(self, items):
-        return sum(items, [])
+        assert len(items) == 1
+        return items[0]
 
     def expr_mult(self, items):
         assert len(items) == 2
-        return items + [Instr('mul')]
+        return binary_op('mul', items)
 
     def expr_div(self, items):
         assert len(items) == 2
+        return binary_op('div', items)
+
         return items + [Instr('div')]
 
     def unary_expr(self, items):
@@ -263,9 +355,10 @@ class MyC(Transformer):
     def value(self, items):
         assert len(items) == 1
         if isinstance(items[0], Token):
-            return [Instr('pushi', items[0].value)]
+            t = get_literal_typespec(items[0].value)
+            return t, [Instr('pushi' + t, items[0].value)]
         elif isinstance(items[0], Var):
-            return [Instr('pushl', items[0].qual_name())]
+            return items[0].typespec, [Instr('pushl', items[0].qual_name())]
         else:
             raise RuntimeError('Got something unexpected.')
 
@@ -280,7 +373,8 @@ class MyC(Transformer):
 
     def negation(self, items):
         assert len(items) == 1
-        return items + [Instr('neg')]
+        t, instrs = items[0]
+        return t, instrs + [Instr('neg')]
 
 
 with open('qpybasic.ebnf') as f:
@@ -289,9 +383,9 @@ with open('qpybasic.ebnf') as f:
 prog = r"""
 start:
 x = 100:let y = 200
-z = "foo"
-z2 = "bar"
-foo = z + z2
+z$ = "foo"
+z2$ = "bar"
+foo$ = z$ + z2$
 
 if x < y then
     print "less"
@@ -301,7 +395,7 @@ end if
 
 xyz: cls
 print
-10 print x; y; -(x + y*2), foo
+10 print x; y; -(x + y*2), foo$
 print 1, 2, z
 
 for i = 1 to 10
