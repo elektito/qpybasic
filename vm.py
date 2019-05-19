75182dd02f7a1719152a6da319523f43ab1e2184
Mostafa Razavi <mostafa@sepent.com>
2019-05-19 02:43:58 +0200

    Refactoring: use asm module in vm.

--- vm.py
+++ vm.py
@@ -2,6 +2,7 @@
 
 import struct
 import logging
+import asm
 import logging.config
 from mmap import mmap
 
@@ -33,48 +34,22 @@ class Machine:
 
 
     def step(self):
+        def safe_name(s):
+            if s.endswith('%'):
+                return s[:-1] + '_integer'
+            elif s.endswith('&'):
+                return s[:-1] + '_long'
+            elif s.endswith('!'):
+                return s[:-1] + '_single'
+            elif s.endswith('#'):
+                return s[:-1] + '_double'
+            elif s.endswith('$'):
+                return s[:-1] + '_pointer'
+            else:
+                return s
         self.mem.seek(self.ip)
         opcode = self.mem.read_byte()
-        opname = {
-            0x01: 'add_integer',
-            0x02: 'add_long',
-            0x03: 'add_single',
-            0x05: 'call',
-            0x06: 'conv_int_long',
-            0x07: 'conv_int_single',
-            0x0c: 'conv_single_int',
-            0x13: 'end',
-            0x14: 'frame',
-            0x17: 'gt',
-            0x1b: 'le',
-            0x18: 'jmp',
-            0x19: 'jmpf',
-            0x1a: 'jmpt',
-            0x1c: 'lt',
-            0x1f: 'mul_single',
-            0x22: 'neg_int',
-            0x24: 'neg_single',
-            0x26: 'pushi_int',
-            0x28: 'pushi_single',
-            0x2a: 'pushi_string',
-            0x2c: 'readf2',
-            0x2d: 'readf4',
-            0x2e: 'sub_int',
-            0x30: 'sub_single',
-            0x32: 'syscall',
-            0x34: 'writef2',
-            0x35: 'writef4',
-            0x37: 'ret',
-            0x38: 'unframe',
-            0x3b: 'readi2',
-            0x3c: 'readi4',
-            0x40: 'writei4',
-            0x42: 'pushfp',
-            0x44: 'dup2',
-            0x46: 'sgn_integer',
-            0x4a: 'unframe_r',
-            0x4b: 'ret_r',
-        }[opcode]
+        opname = safe_name(asm.opcode_to_instr[opcode].name)
         n = getattr(self, f'exec_{opname}')()
         if isinstance(n, Jump):
             # return value is an absolute jump.
@@ -151,7 +126,7 @@ class Machine:
         return 0
 
 
-    def exec_conv_single_int(self):
+    def exec_conv_single_integer(self):
         value = self.pop(4)
         value, = struct.unpack('>f', value)
         value = int(value)
@@ -278,7 +253,7 @@ class Machine:
         return 0
 
 
-    def exec_neg_int(self):
+    def exec_neg_integer(self):
         x = self.pop(2)
         x, = struct.unpack('>h', x)
         result = -x
@@ -297,7 +272,7 @@ class Machine:
         return 2
 
 
-    def exec_pushi_int(self):
+    def exec_pushi_integer(self):
         value = self.mem.read(2)
         self.push(value)
         logger.debug('EXEC: pushi%')
@@ -391,7 +366,7 @@ class Machine:
         return 0
 
 
-    def exec_sub_int(self):
+    def exec_sub_integer(self):
         y = self.pop(2)
         x = self.pop(2)
         y, = struct.unpack('>h', y)
