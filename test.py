52b35c00bbc87ebcabe6bb9d8cbfe65333443a43
Mostafa Razavi <mostafa@sepent.com>
2019-05-29 16:06:06 +0000

    Better error reporting in test module.

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import logging
 import traceback
+import unittest
 from compiler import Compiler, CompileError
 from vm import Machine
 
 logger = logging.getLogger(__name__)
+
+# we are going to create this phony test case, so that we can use its
+# assert* methods, which are much nicer than the vanilla assert
+# statement.
+tc = unittest.TestCase()
 
 class TestCls:
     code = """
@@ -14,6 +20,7 @@ cls
     vevents = [
         ('cls',)
     ]
+
 
 class TestPrint1:
     code = """
@@ -36,9 +43,9 @@ def run_test_case(name, case):
     c = Compiler()
     try:
         module = c.compile(case.code)
-    except CompileError:
+    except CompileError as e:
         event_handler(('error', e.code))
-    assert events == case.cevents
+    tc.assertEqual(events, case.cevents)
 
     events = []
     machine = Machine.load(module)
@@ -46,7 +53,7 @@ def run_test_case(name, case):
     machine.launch()
     machine.shutdown()
 
-    assert events == case.vevents
+    tc.assertEqual(events, case.vevents)
 
 
 def main():
@@ -57,15 +64,26 @@ def main():
         if name.startswith('Test') and isinstance(value, type):
             try:
                 run_test_case(name, value)
-            except Exception:
-                failed.append(name)
-                traceback.print_exc()
+            except Exception as e:
+                failed.append((name, e))
+                if isinstance(e, AssertionError):
+                    print('F', end='')
+                else:
+                    print('E', end='')
             else:
                 success.append(name)
+                print('.', end='')
 
+    print()
     if len(failed) == 0:
         print(f'All {len(success)} test case(s) ran successfully.')
     else:
+        print('Failures:\n')
+        for name, exc in failed:
+            print(f'Failed test case: {name}')
+            print('Exception:')
+            traceback.print_exception(type(exc), exc, exc.__traceback__)
+        print('---\n')
         total = len(failed) + len(success)
         print(f'{len(failed)} out of {total} test case(s) failed.')
 
