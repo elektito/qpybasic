4f5b774a37d61e3fd718a42d751c6851a3ea639e
Mostafa Razavi <mostafa@sepent.com>
2019-05-29 16:37:56 +0000

    Add better command-line argument handling.

--- qpybasic.py
+++ qpybasic.py
@@ -1,38 +1,45 @@
+import argparse
+import codecs
 from compiler import Compiler, CompileError
 
 
-with open('qpybasic.ebnf') as f:
-    grammar_text = f.read()
+def main():
+    parser = argparse.ArgumentParser(
+        description='QpyBASIC')
 
-prog = r"""
-'sample code
-declare function fib%(n%)
+    parser.add_argument('input_file', help='Input source code.')
+    parser.add_argument(
+        '--encoding', '-e', default='latin-1',
+        help='The encoding of the input file. Defaults to latin-1.')
+    parser.add_argument(
+        '--dump-asm', '-d', action='store_true', default=False,
+        help='Dump assembly output to stdout.')
+    parser.add_argument(
+        '--output', '-o', default='a.mod',
+        help='The output file. Defaults to "a.mod".')
 
-dim i as integer
+    args = parser.parse_args()
 
-for i = 1 to 10
-   print "fib"; i; fib%(i)
-next
+    with codecs.open(args.input_file, encoding=args.encoding) as f:
+        source = f.read()
 
-function fib%(n%)
-   if n% < 3 then
-      fib% = 1
-   else
-      fib% = fib%(n%-2) + fib%(n%-1)
-   end if
-end function
+    with open('qpybasic.ebnf') as f:
+        grammar_text = f.read()
 
-"""
+    c = Compiler()
+    try:
+        module = c.compile(source)
+    except CompileError as e:
+        print('COMPILE ERROR:', e)
+        exit(1)
 
-c = Compiler()
-try:
-    module = c.compile(prog)
-except CompileError as e:
-    print('COMPILE ERROR:', e)
-    exit(1)
+    if args.dump_asm:
+        for i in c.instrs:
+            print(i)
 
-for i in c.instrs:
-    print(i)
+    with open(args.output, 'wb') as f:
+        f.write(module.dump())
 
-with open('foo.mod', 'wb') as f:
-    f.write(module.dump())
+
+if __name__ == '__main__':
+    main()
